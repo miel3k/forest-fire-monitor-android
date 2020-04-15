4ed97db7b0908d302e3411d00c9e073e6c1d0fae
miel3k <kuba.mielczarek96@icloud.com>
2020-04-15 23:47:29 +0200

    Setup edit area action mode

--- app/src/main/java/com/tp/forestfiremonitor/map/MapActivity.kt
+++ app/src/main/java/com/tp/forestfiremonitor/map/MapActivity.kt
@@ -1,7 +1,13 @@
 package com.tp.forestfiremonitor.map
 
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.view.ActionMode
+import androidx.lifecycle.Observer
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -16,17 +22,87 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     private lateinit var map: GoogleMap
     private val viewModel by viewModel<MapViewModel>()
 
+    private var editAreaActionMode: ActionMode? = null
+    private val editAreaActionModeCallback = object : ActionMode.Callback {
+        override fun onActionItemClicked(mode: ActionMode, item: MenuItem): Boolean {
+            return when (item.itemId) {
+                R.id.menu_item_save -> {
+                    viewModel.saveEditArea()
+                    true
+                }
+                else -> true
+            }
+        }
+
+        override fun onCreateActionMode(mode: ActionMode, menu: Menu): Boolean {
+            mode.menuInflater.inflate(R.menu.menu_edit_area_mode, menu)
+            return true
+        }
+
+        override fun onPrepareActionMode(mode: ActionMode, menu: Menu): Boolean {
+            return false
+        }
+
+        override fun onDestroyActionMode(mode: ActionMode) {
+            editAreaActionMode = null
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        setupIsEditAreaModeEnabledObserver()
+        setupCloseActionModeButton()
     }
-    
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_map, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.menu_item_edit_area_mode -> {
+                viewModel.openEditAreaMode()
+                true
+            }
+            else -> false
+        }
+    }
+
+    override fun dispatchKeyEvent(event: KeyEvent): Boolean {
+        editAreaActionMode?.let {
+            if (event.keyCode == KeyEvent.KEYCODE_BACK && event.action == KeyEvent.ACTION_DOWN) {
+                viewModel.closeEditAreaMode()
+                return true
+            }
+        }
+        return super.dispatchKeyEvent(event)
+    }
+
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
         val sydney = LatLng(-34.0, 151.0)
         map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+    private fun setupIsEditAreaModeEnabledObserver() {
+        viewModel.isEditAreaModeEnabled.observe(this, Observer {
+            if (it) {
+                editAreaActionMode = startSupportActionMode(editAreaActionModeCallback)
+            } else {
+                editAreaActionMode?.finish()
+            }
+        })
+    }
+
+    private fun setupCloseActionModeButton() {
+        val closeButton = findViewById<View>(androidx.appcompat.R.id.action_mode_close_button)
+        closeButton?.setOnClickListener {
+            viewModel.closeEditAreaMode()
+        }
     }
 }
--- app/src/main/java/com/tp/forestfiremonitor/map/MapViewModel.kt
+++ app/src/main/java/com/tp/forestfiremonitor/map/MapViewModel.kt
@@ -1,6 +1,21 @@
 package com.tp.forestfiremonitor.map
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MapViewModel : ViewModel() {
+
+    val isEditAreaModeEnabled = MutableLiveData<Boolean>().apply { value = false }
+
+    fun openEditAreaMode() {
+        isEditAreaModeEnabled.value = true
+    }
+
+    fun closeEditAreaMode() {
+        isEditAreaModeEnabled.value = false
+    }
+
+    fun saveEditArea() {
+
+    }
 }
